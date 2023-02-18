d20c5cc83b5c714d3ebc1002dfe59b1e30160cb0
love <danielove5556@example.com>
2023-02-18 05:18:35 -0800

    task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print 1 9 with putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 48;
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar(10);
+return (0);
+}
