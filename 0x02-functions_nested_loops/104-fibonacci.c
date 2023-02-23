5539246498b24fc557d1b133bc0fd69bdb612de2
love <danielove5556@example.com>
2023-02-22 17:26:07 -0800

    task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,8 @@ unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
 for (count = 0; count < 92; count++)
 {
-sum printf("%lu, ", sum);
+sum = fib1 + fib2;
+printf("%lu, ", sum);
 fib1 = fib2;
 fib2 = sum;
 }
