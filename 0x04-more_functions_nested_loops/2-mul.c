184aa4a9d9a1b0c8d0bed674e05fb623ff277f14
love <danielove5556@example.com>
2023-02-23 16:13:45 -0800

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Adds two integers.
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
