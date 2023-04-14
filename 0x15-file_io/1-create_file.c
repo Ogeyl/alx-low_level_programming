01c4bd9e89746f0291583994c4fa701b7a426024
Love <danielove5556@gmail.com>
2023-04-14 15:55:32 -0700

    fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *  create_file -creates an array of chars, and initializes
+ * create_file - creates an array of chars, and initializes
  *
  * @text_content: is a NULL terminated string to write to the file
  * @filename: is the name of the file to create
@@ -10,7 +10,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, len;
+	int fd, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
 
 	if (fd == -1 || wr == -1)
